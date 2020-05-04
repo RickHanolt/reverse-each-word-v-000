ace76037a01cccb6caf0a253d57d8f703110608d
Richard Hanolt <rhanolt@gmail.com>
2020-05-04 20:57:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,4 @@
 def reverse_each_word(sentence)
-  backwards_words = Array.new
-  sentence_array = sentence.split(" ")
-  sentence_array.each do |word|
-    backwards_words << word.reverse
-  end
-  backwards_words.join(" ")
-end
-
-def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   (sentence_array.collect{|word| word.reverse}).join(" ")
 end
